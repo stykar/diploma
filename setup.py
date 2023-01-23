1dbd5586b3d0e8d1e95a39938f89bcf4fa886563
stykar <stekarageorgis@gmail.com>
2023-01-23 22:57:02 +0200

    initial

---
+++ setup.py
@@ -0,0 +1,15 @@
+import re
+import setuptools
+from os import path
+
+requirements_file = path.join(path.dirname(_file_), "requirements.in")
+requirements = [r for r in open(requirements_file).read().split("\n") if not re.match(r"^\-", r)]
+
+setuptools.setup(
+  name="diploma",
+  version="0.1",
+  url="https://github.com/stykar/diploma",
+  packages=setuptools.find_packages(),
+  install_requires=requirements,  # dependencies specified in requirements.in
+  description='Diploma thesis',
+)
