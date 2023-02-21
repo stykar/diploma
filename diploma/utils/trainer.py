caf9d127c6697d2ab69ab330c6112e61eb8e7a32
kostasang <kostas.ang97@gmail.com>
2023-02-21 22:54:22 +0200

    Fix bugs

--- diploma/utils/trainer.py
+++ diploma/utils/trainer.py
@@ -1,16 +1,18 @@
 import torch
-import numpy as np
 import torch.nn as nn
 import torch.optim as optim
 from torch.utils.data import DataLoader
 from tqdm import tqdm
+from collections import defaultdict
+from sklearn.metrics import accuracy_score, recall_score, precision_score
+
 
 def train_model(
     model: nn.Module,
     train_dataloader: DataLoader,
     validation_dataloader: DataLoader,
     optimizer: optim.Optimizer,
-    loss_functions : list,
+    loss_functions: list,
     epochs: int,
     evaluation_steps: int,
     best_model_path: str
@@ -18,16 +20,14 @@ def train_model(
     """
     Implements training loop for the model
     """
-    train_losses = []
-    validation_losses = []
-    validation_accs = []
+    metrics = defaultdict(list)
     model.train()
     global_step = 0
     best_score = 999999
     for _ in tqdm(range(epochs)):
         training_step = 0
         sum_loss = 0
-        for x_batch, y_batch in train_dataloader:    
+        for x_batch, y_batch in train_dataloader:
             optimizer.zero_grad()
             y_pred = model(x_batch)
             loss = 0
@@ -38,42 +38,54 @@ def train_model(
             sum_loss += loss.item()
             training_step += 1
             global_step += 1
-            if evaluation_steps > 0 and training_step % evaluation_steps == 0:
-                validation_loss, validation_acc = evaluate_model(
+            if evaluation_steps > 0 and global_step % evaluation_steps == 0:
+                evaluation_results = evaluate_model(
                     model=model,
                     validation_loader=validation_dataloader,
                     loss_functions=loss_functions
                 )
-                train_losses.append(sum_loss/training_step)
-                validation_losses.append(validation_loss)
-                validation_accs.append(validation_acc)
-                print(f'Train loss: {train_losses[-1]} -- Validation loss: {validation_losses[-1]} -- Validation accuracy: {validation_accs[-1]}')
-                if validation_loss < best_score:
-                    best_score = validation_loss
+                metrics['steps'].append(global_step)
+                metrics['train_loss'].append(sum_loss/training_step)
+                for metric, value in evaluation_results.items():
+                    metrics[metric].append(value)
+
+                print(
+                    f'Train loss: {metrics["train_loss"][-1]}',
+                    f'-- Validation loss: {metrics["val_loss"][-1]}'
+                )
+                if metrics["val_loss"][-1] < best_score:
+                    best_score = metrics["val_loss"][-1]
                     torch.save(model.state_dict(), best_model_path)
-    
-    return train_losses, validation_losses, validation_accs
+    return metrics
+
 
 @torch.inference_mode()
 def evaluate_model(
     model: nn.Module,
     validation_loader: DataLoader,
     loss_functions
-) -> tuple[float, float]:
+) -> dict[str, float]:
     """
-    Evaluates model on evaluation dataloader and returns (loss, eval accuracy)
+    Evaluates model on evaluation dataloader and returns dict of metrics
     """
-    total_loss = 0 
+    total_loss = 0
     model.eval()
-    preds = []
+    predictions = []
+    labels = []
     for idx, batch in enumerate(validation_loader):
         x_batch, y_batch = batch
         y_pred = model(x_batch)
         loss = 0
         for loss_func in loss_functions:
             loss += loss_func(y_pred, y_batch)
-            preds += torch.abs(torch.argmax(y_pred, dim=1) - y_batch[:,1]).tolist()
-
         total_loss += loss.item()
+        predictions += torch.argmax(y_pred, dim=-1).tolist()
+        labels += y_batch[:, 1].tolist()
     model.train()
-    return total_loss / (idx+1), (len(preds) - np.asarray(preds).sum()) / len(preds)
+    total_loss = total_loss / (idx + 1)
+    return {
+        'val_loss': total_loss,
+        'val_accuracy': accuracy_score(y_true=predictions, y_pred=labels),
+        'val_recall': recall_score(y_true=predictions, y_pred=labels, zero_division=0),
+        'val_precision': precision_score(y_true=predictions, y_pred=labels, zero_division=0)
+    }
--- scripts/run_train_mlp.py
+++ scripts/run_train_mlp.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import matplotlib.pyplot as plt
+import numpy as np
 from torch.utils.data import DataLoader
 from torch.optim import Adam
 from torch.nn import BCEWithLogitsLoss
@@ -14,8 +15,8 @@ if __name__ == "__main__":
 
     df_train_LOX = pd.read_csv('data/training_set_LOX.csv')
     df_train_ANT = pd.read_csv('data/training_set_antioxidant.csv')
-    df_train_LOX = df_train_LOX.drop(columns = ['Title'], axis = 1)
-    df_train_ANT = df_train_ANT.drop(columns = ['Title'], axis = 1)
+    df_train_LOX = df_train_LOX.drop(columns=['Title'], axis=1)
+    df_train_ANT = df_train_ANT.drop(columns=['Title'], axis=1)
 
     mean_values_LOX = df_train_LOX.mean()
     mean_values_ANT = df_train_ANT.mean()
@@ -27,26 +28,27 @@ if __name__ == "__main__":
     train_set = pd.concat(frames)
 
     x = train_set.loc[:, train_set.columns != 'class'].to_numpy()
-    y = train_set.iloc[:,0].to_numpy()
-    x_train, x_validation, y_train, y_validation = train_test_split(x, y, test_size=0.2, random_state=0)
+    y = train_set.iloc[:, 0].to_numpy()
+    x_train, x_validation, y_train, y_validation = train_test_split(
+        x, y,
+        test_size=0.2,
+        random_state=0
+    )
 
     scaler = preprocessing.MinMaxScaler()
     x_train = scaler.fit_transform(x_train)
     x_validation = scaler.transform(x_validation)
 
+    train_dataset = PharmaDataset(features=x_train, labels=y_train)
+    val_dataset = PharmaDataset(features=x_validation, labels=y_validation)
+
     train_loader = DataLoader(
-        dataset=PharmaDataset(
-            features=x_train,
-            labels=y_train,
-        ),
+        dataset=train_dataset,
         batch_size=128,
         shuffle=True
     )
     validation_loader = DataLoader(
-        dataset=PharmaDataset(
-            features=x_validation,
-            labels=y_validation
-        ),
+        dataset=val_dataset,
         batch_size=4
     )
 
@@ -59,25 +61,29 @@ if __name__ == "__main__":
         params=model.parameters(),
         lr=1e-03
     )
-    loss_function = BCEWithLogitsLoss()
-
-    train_losess, val_losses, val_accs = train_model(
+    metrics = train_model(
         model=model,
         train_dataloader=train_loader,
         validation_dataloader=validation_loader,
         optimizer=optimizer,
-        loss_functions=[loss_function],
-        epochs=30,
+        loss_functions=[BCEWithLogitsLoss()],
+        epochs=10,
         evaluation_steps=25,
         best_model_path='models/nn.pt'
     )
 
-    steps = range(25, (len(train_losess)+1)*25, 25)
-    plt.plot(steps, train_losess)
-    plt.plot(steps, val_losses)
-    plt.plot(steps, val_accs)
+    steps = metrics.pop('steps')
+    for metric in metrics.values():
+        plt.plot(steps, metric)
     plt.grid()
     plt.title('Training procedure')
     plt.xlabel('Step')
-    plt.legend(['Train loss', 'Validation loss', 'Validation accuracy'])
+    plt.legend(metrics.keys())
     plt.savefig('results/losses', dpi=500)
+
+    df = pd.read_csv('data/test_set.csv').drop(columns=['Title'], axis=1)
+    test = df.to_numpy()
+    test = scaler.transform(test)
+    probs = model.predict(test)
+    np.set_printoptions(suppress=True)
+    print(probs)
